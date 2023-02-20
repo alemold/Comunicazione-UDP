4ee5ace9e43cf96c806f5805f91db331f3e0661c
alemold <bagnettialessio@gmail.com>
2023-02-20 13:24:22 +0100

    primo commit

---
+++ Server.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket server = new DatagramSocket(2000);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+
+            while(true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            }
+
+
+
+        } catch(SocketException e) {
+            System.err.println(e);
+        }
+
+    }
+}
