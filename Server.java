acfc1f689a00a5e28e76c0c0a99218d2206393a5
alemold <bagnettialessio@gmail.com>
2023-02-20 13:38:19 +0100

    server funzionante

--- Server.java
+++ Server.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
 
 public class Server {
@@ -9,16 +10,25 @@ public class Server {
             DatagramSocket server = new DatagramSocket(2000);
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
+            //PRENDI DAL LIBRO COMANDI
 
             while(true) {
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                try {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    server.receive(receivePacket);
+                    String message = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                    InetAddress IPAddress = receivePacket.getAddress();
+                    int port = receivePacket.getPort();
+                    String capitalizedMessage = message.toUpperCase();
+                    sendData = capitalizedMessage.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                    server.send(sendPacket);
+                } catch(IOException e) {
+                    System.err.println(e);
+                }
             }
-
-
-
         } catch(SocketException e) {
             System.err.println(e);
         }
-
     }
 }
